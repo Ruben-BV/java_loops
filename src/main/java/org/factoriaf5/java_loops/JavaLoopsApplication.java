7bd82ee21910a3a01f7b5d2852b915dc1c438679
Rubén Blanco <r.blanco.3d@gmail.com>
2024-10-18 22:30:38 +0200

    Estructura Springboot creada

---
+++ src/main/java/org/factoriaf5/java_loops/JavaLoopsApplication.java
@@ -0,0 +1,13 @@
+package org.factoriaf5.java_loops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLoopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaLoopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/factoriaf5/java_loops/JavaLoopsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.factoriaf5.java_loops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLoopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
