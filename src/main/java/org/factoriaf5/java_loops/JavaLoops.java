1f225ce58c450e5ad3f7d34966807ca4229519f5
Rubén Blanco <r.blanco.3d@gmail.com>
2024-10-18 23:56:25 +0200

    testTablaDeMultiplicar - Coverage: 100%

--- src/main/java/org/factoriaf5/java_loops/JavaLoops.java
+++ src/main/java/org/factoriaf5/java_loops/JavaLoops.java
@@ -5,7 +5,7 @@ public class JavaLoops {
     public String tablaDeMultiplicar(int n) {
         
         String[] tabla = new String[10];
-        
+
         for (int i=0; i<tabla.length; i++) {
             tabla[i] = (n+" x "+(i+1)+" = "+n*(i+1));
         }
--- src/test/java/org/factoriaf5/java_loops/JavaLoopsTest.java
+++ src/test/java/org/factoriaf5/java_loops/JavaLoopsTest.java
@@ -1,6 +1,30 @@
 package org.factoriaf5.java_loops;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class JavaLoopsTest {
     
+    private JavaLoops javaLoops;
 
+    @BeforeEach
+    public void init() {
+        this.javaLoops = new JavaLoops();
+    }
+
+
+    @Test
+    public void testTablaDeMultiplicar() {
+        
+        int num1 = 4;
+
+        // When - Cuando
+        String result = javaLoops.tablaDeMultiplicar(num1);
+
+        // Then - Entonces
+        assertEquals("4 x 1 = 4\n4 x 2 = 8\n4 x 3 = 12\n4 x 4 = 16\n4 x 5 = 20\n4 x 6 = 24\n4 x 7 = 28\n4 x 8 = 32\n4 x 9 = 36\n4 x 10 = 40", result);
+        
+    }
 }
